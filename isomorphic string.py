74402702c1c336e115026f42d52bd23acd95c2be
dharanigiri3017-glitch <dharanigiri3017@gmail.com>
2026-07-21 21:04:28 +0530

    Create isomorphic string.py

---
+++ isomorphic string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        mapST = {}
+        mapTS = {}
+
+        for i in range(len(s)):
+            c1, c2 = s[i], t[i]
+
+            if c1 in mapST and mapST[c1] != c2:
+                return False
+
+            if c2 in mapTS and mapTS[c2] != c1:
+                return False
+
+            mapST[c1] = c2
+            mapTS[c2] = c1
+
+        return True
