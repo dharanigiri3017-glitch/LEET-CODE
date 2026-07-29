d0871841fa92a46a0c67055461ee662cd2bc48de
dharanigiri3017-glitch <dharanigiri3017@gmail.com>
2026-07-29 21:03:10 +0530

    Create covert a number into hexadecimal.py

---
+++ covert a number into hexadecimal.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def toHex(self, num):
+        
+        if num == 0:
+            return "0"
+        if num < 0:
+            num += 2 ** 32
+
+        hex_chars = "0123456789abcdef"
+        result = ""
+
+        while num > 0:
+            result = hex_chars[num % 16] + result
+            num //= 16
+
+        return result
+        
