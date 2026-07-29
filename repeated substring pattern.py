00592604a474ca9e3925f3adcc651c5b99dd9a25
dharanigiri3017-glitch <dharanigiri3017@gmail.com>
2026-07-29 21:16:16 +0530

    Create repeated substring pattern.py

---
+++ repeated substring pattern.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        
+        n = len(s)
+
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                if s[:i] * (n // i) == s:
+                    return True
+
+        return False
+        
