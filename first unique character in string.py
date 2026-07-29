c1727ff37fd2e4ac02b1e90223284be7ce73ced8
dharanigiri3017-glitch <dharanigiri3017@gmail.com>
2026-07-29 20:50:41 +0530

    Create first unique character in string.py

---
+++ first unique character in string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+      
+        count = Counter(s)
+
+        for i, ch in enumerate(s):
+            if count[ch] == 1:
+                return i
+
+        return -1
