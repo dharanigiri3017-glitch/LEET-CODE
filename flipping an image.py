2b905fb429ab62224f4afd8faf5cb9d8ea88f70b
Dharanigiri <dharanigiri3017@gmail.com>
2026-09-22 21:39:55 +0530

    Create flipping an image.py

---
+++ flipping an image.py
@@ -0,0 +1,12 @@
+class Solution:
+    def flipAndInvertImage(self, image):
+        for row in image:
+            row.reverse()
+
+            for i in range(len(row)):
+                if row[i] == 0:
+                    row[i] = 1
+                else:
+                    row[i] = 0
+
+        return image
