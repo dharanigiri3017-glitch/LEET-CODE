3c49063442c01786e23eaac743d2a3da81629d47
Dharanigiri <dharanigiri3017@gmail.com>
2026-09-26 19:34:13 +0530

    Create string matching in an array.py

---
+++ string matching in an array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stringMatching(self, words):
+        result = []
+
+        for word in words:
+            for other in words:
+                if word != other and word in other:
+                    result.append(word)
+                    break
+
+        return result
